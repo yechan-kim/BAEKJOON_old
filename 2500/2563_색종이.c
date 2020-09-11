7e2eb90f24a91472bf8b0409257c843a2965e17b
KimYeChan <tisckd@naver.com>
2020-09-11 10:06:02 +0900

    unsolved

---
+++ 2500/2563_색종이.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int base[101][101] = {0};
+    int i, j, a = 0, b = 0, c = 0;
+    scanf("%d", &c);
+    for (i = 0; i < c; i++)
+    {
+        scanf("%d %d", &a, &b);
+        
+    }
+}
